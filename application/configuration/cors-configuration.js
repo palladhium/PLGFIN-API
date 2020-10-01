03801b42aae458759fcb608bbfce68bde81c7258
Ununpentium <ununpentium@outlook.com.br>
2020-10-01 15:13:35 -0300

    PLGFIN-08152020100136
    - Implementa ajuste na API das configurações do CORS

--- application/configuration/cors-configuration.js
+++ application/configuration/cors-configuration.js
@@ -1,5 +1,5 @@
 module.exports = function(request, response, next) {
-    response.header("Acess-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
     response.header("Acess-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
